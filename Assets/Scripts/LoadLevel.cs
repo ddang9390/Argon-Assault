3ae8fd63e7eab587e9d827929887ac6b1a1d4120
ddang9390 <dnld1ng@yahoo.com>
2018-06-17 21:59:28 -0400

    Made spaceship and splash screen

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("loadGame", 10f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void loadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
