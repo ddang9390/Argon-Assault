d05fdacd050df529c4821c3b0ee81e3590c0019c
ddang9390 <dnld1ng@yahoo.com>
2018-06-18 12:37:33 -0400

    Ship can move through input

---
+++ Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerShip : MonoBehaviour {
+    [Tooltip("In m/s")][SerializeField] float xSpeed = 4f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5f;
+    [Tooltip("In m")] [SerializeField] float yRange = 3f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xOffset;
+        float yOffset;
+
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        xOffset = xSpeed * xThrow * Time.deltaTime;
+        float newXPos = transform.localPosition.x + xOffset;
+        float trueXPos = Mathf.Clamp(newXPos, -xRange, xRange);
+
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        yOffset = xSpeed * yThrow * Time.deltaTime;
+        float newYPos = transform.localPosition.y + yOffset;
+        float trueYPos = Mathf.Clamp(newYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(trueXPos, trueYPos, transform.localPosition.z);
+	}
+}
