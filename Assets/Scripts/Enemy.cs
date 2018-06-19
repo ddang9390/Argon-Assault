ffea9ee67aadb2a3bb628a2d1ba66be9e0fa5dbe
ddang9390 <dnld1ng@yahoo.com>
2018-06-19 15:17:03 -0400

    Enemies can blow up

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
     [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+
     // Use this for initialization
     void Start () {
-		
-	}
+        Collider collider = gameObject.AddComponent<BoxCollider>();
+        collider.isTrigger = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -16,8 +19,9 @@ public class Enemy : MonoBehaviour {
 
     private void OnParticleCollision(GameObject other)
     {
-        Debug.Log("Hit " + gameObject.name);
-        deathFX.SetActive(true);
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+
         Destroy(gameObject);
     }
 
