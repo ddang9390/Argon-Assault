ec86a791ae0760ac1f390e76c45b51746900ef96
ddang9390 <dnld1ng@yahoo.com>
2018-06-19 12:36:24 -0400

    Player ship can blow up

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour {
+    [SerializeField] float levelLoadDelay = 1f;
+    [SerializeField] GameObject explosion;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartDeathSequence();
+    }
+
+    private void StartDeathSequence()
+    {
+        gameObject.SendMessage("OnPlayerDeath");
+        explosion.SetActive(true);
+        Invoke("restartLevel", levelLoadDelay);
+    }
+
+    private void restartLevel() // string referenced
+    {
+        SceneManager.LoadScene(1);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class PlayerShip : MonoBehaviour {
-    [Tooltip("In m/s")][SerializeField] float xSpeed = 20f;
-    [Tooltip("In m")] [SerializeField] float xRange = 5.5f;
-    [Tooltip("In m")] [SerializeField] float yRange = 3.5f;
+public class PlayerController : MonoBehaviour {
+    [Header("General")]
+    [Tooltip("In m/s")][SerializeField] float controlSpeed = 20f;
+    [Tooltip("In m")] [SerializeField] float xRange = 9f;
+    [Tooltip("In m")] [SerializeField] float yRange = 8f;
 
+    [Header("Screen-Position Based")]
     [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float positionYawFactor = 5f;
+
+    [Header("Control-Throw Based")]
     [SerializeField] float controlPitchFactor = -20f;
-    [SerializeField] float positionYawFactor = -5f;
     [SerializeField] float controlRollFactor = -20f;
 
     float yThrow, xThrow;
+    bool isDead = false;
     // Use this for initialization
     void Start () {
 		
@@ -22,8 +27,11 @@ public class PlayerShip : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        ProcessTranslation();
-        ProcessRotation();
+        if (!isDead)
+        {
+            ProcessTranslation();
+            ProcessRotation();
+        }
     }
 
     // Handles translation of ship
@@ -35,12 +43,12 @@ public class PlayerShip : MonoBehaviour {
         yThrow = CrossPlatformInputManager.GetAxis("Vertical");
         xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
 
-        xOffset = xSpeed * xThrow * Time.deltaTime;
+        xOffset = controlSpeed * xThrow * Time.deltaTime;
         float newXPos = transform.localPosition.x + xOffset;
         float trueXPos = Mathf.Clamp(newXPos, -xRange, xRange); // Keeps ship within screen's x-range
 
         
-        yOffset = xSpeed * yThrow * Time.deltaTime;
+        yOffset = controlSpeed * yThrow * Time.deltaTime;
         float newYPos = transform.localPosition.y + yOffset;
         float trueYPos = Mathf.Clamp(newYPos, -yRange, yRange); // Keeps ship within screen's y-range
 
@@ -57,13 +65,10 @@ public class PlayerShip : MonoBehaviour {
         transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnPlayerDeath()
     {
-        Debug.Log("I'm hit");
+        Debug.Log("i dead");
+        isDead = true;
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("trigger");
-    }
 }
