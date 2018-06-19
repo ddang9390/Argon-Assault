0a8f5dcabe0b68fd7cbafc97e7b7d07ef77c414b
ddang9390 <dnld1ng@yahoo.com>
2018-06-19 14:24:39 -0400

    Particles can collide with enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    [SerializeField] GameObject deathFX;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("Hit " + gameObject.name);
+        deathFX.SetActive(true);
+        Destroy(gameObject);
+    }
+
+}
